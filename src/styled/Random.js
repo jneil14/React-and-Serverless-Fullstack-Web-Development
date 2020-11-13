734c2ffcd985879f8bd99948dd003cd0d27c2ef8
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-11-12 21:13:08 -0800

    added styled Home and Game components

--- src/styled/Random.js
+++ src/styled/Random.js
@@ -3,3 +3,15 @@ import styled from "styled-components";
 export const Accent = styled.span`
   color: #e16365;
 `;
+
+export const StyledTitle = styled.h1 `
+    margin-top: 3rem;
+    margin-bottom: 2rem;
+    font-size: 4.2rem;
+    text-align: center;
+`;
+
+export const Strong = styled.strong `
+  font-weight: bold;
+  
+`
