43b16bfa28f498b66b5a7fe0771aa1cc0b96908d
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-11-12 16:12:51 -0800

    Initialized base components and global styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Game from "./pages/Game";
 import GameOver from "./pages/GameOver";
 import HighScores from "./pages/HighScores";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-
-
+import { Container } from "./styled/Container";
+import { Main } from "./styled/Main";
+import Global from "./styled/Global";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/game" component={Game} />
-        <Route path="/highScores" component={HighScores} />
-        <Route path="/gameOver" component={GameOver} />
-        <Route path="/" component={Home} />
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
 
-      </Switch>
+          <Switch>
+            <Route path="/game" component={Game} />
+            <Route path="/highScores" component={HighScores} />
+            <Route path="/gameOver" component={GameOver} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
     </Router>
   );
 }
