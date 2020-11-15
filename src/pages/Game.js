2a34a30f8d914a39544db7f3bdffe84df1f1a394
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-11-15 14:02:55 -0800

    Timer functionality added

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import {
   StyledGame,
   StyledScore,
@@ -7,16 +7,61 @@ import {
 } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
-export default function Game() {
-    return (
-      <StyledGame>
-        <StyledScore>
-          Score: <Strong>0</Strong>
-        </StyledScore>
-        <StyledCharacter>A</StyledCharacter>
-        <StyledTimer>
-          Time: <Strong>00: 000</Strong>
-        </StyledTimer>
-      </StyledGame>
-    );
+export default function Game({ history }) {
+  const [score, setScore] = useState(1);
+
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = startTime => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMSStr = ("0000" + msPassedStr).slice(-5);
+    // 00000 = first 2 nums are the seconds, last 3 are the ms that have passed
+
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSStr.substring(0, 2)) - 1;
+    const updatedMS =
+      1000 - parseInt(formattedMSStr.substring(formattedMSStr.length - 3));
+
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+
+    setMs(addLeadingZeros(updatedMS, 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push("/gameOver");
+    }
+  }, [seconds, ms, history]);
+
+  return (
+    <StyledGame>
+      <StyledScore>
+        Score: <Strong>{score}</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time:{" "}
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
+      </StyledTimer>
+    </StyledGame>
+  );
 }
