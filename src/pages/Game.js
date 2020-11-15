6b06883661eea9265f7b0be49136fd2e1650ae86
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-11-15 14:14:37 -0800

    Started user input key listening and handling functionality

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -10,7 +10,7 @@ import { Strong } from "../styled/Random";
 export default function Game({ history }) {
   const [score, setScore] = useState(1);
 
-  const MAX_SECONDS = 5;
+  const MAX_SECONDS = 90;
   const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
@@ -49,6 +49,20 @@ export default function Game({ history }) {
       history.push("/gameOver");
     }
   }, [seconds, ms, history]);
+
+
+const keyUpHandler = (e) => {
+    console.log(e.key);
+}
+
+useEffect(() => {
+     document.addEventListener('keyup', keyUpHandler);
+     return () => {
+         document.removeEventListener('keyup', keyUpHandler);
+     }
+}, []);
+
+
 
   return (
     <StyledGame>
