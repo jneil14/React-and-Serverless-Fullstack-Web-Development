753f7c23e503207544524b5e53d7838da564188d
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-11-29 16:36:05 -0800

    Checking in first serverless function

---
+++ functions/helloWorld.js
@@ -0,0 +1,9 @@
+exports.handler = (event, context, callback) => {
+
+    return {
+        statusCode: 200,
+        body:  JSON.stringify({
+            msg: "Hello World!",
+        })
+    };
+};
