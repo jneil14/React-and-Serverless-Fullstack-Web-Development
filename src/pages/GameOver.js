426b2125151c5338fa8829de8865657514859a6d
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-12-05 17:49:22 -0800

    Saving high score in gameOver

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { useScore } from '../contexts/ScoreContext';
-import { StyledLink } from '../styled/Navbar';
+import React, { useEffect, useState } from "react";
+import { useScore } from "../contexts/ScoreContext";
+import { StyledLink } from "../styled/Navbar";
+import { StyledCharacter } from "../styled/Game";
 
 export default function GameOver({ history }) {
-    const [score] = useScore();
+  const [score] = useScore();
+  const [scoreMessage, setScoreMessage] = useState("");
+  if (score === -1) {
+    history.push("/");
+  }
 
-    if(score === -1) {
-        history.push('/')
-    }
-    return(
-        <div>
-            <h1>Game Over</h1>
-            <p>{score}</p>
-            <StyledLink to="/">Go Home</StyledLink>
-            <StyledLink to="/game">Play Again?</StyledLink>
-        </div>
-    )
-} 
+  useEffect(() => {
+    const saveHighScore = async () => {
+      try {
+        const options = {
+          method: "POST",
+          body: JSON.stringify({ name: "Jhong", score })
+        };
+        const res = await fetch("/.netlify/functions/saveHighScore", options);
+        const data = await res.json();
+        if(data.id) {
+            setScoreMessage("Congrats!! You got a score worthy to be in the list!")
+        }
+        else {
+            setScoreMessage("Sorry, your score didn't quite make it to the list. Keep grinding.")
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    saveHighScore();
+  }, []);
+
+  return (
+    <div>
+      <h1>Game Over</h1>
+      <StyledCharacter>{score}</StyledCharacter>
+      <p>{scoreMessage}</p>
+      <StyledLink to="/">Go Home</StyledLink>
+      <StyledLink to="/game">Play Again?</StyledLink>
+    </div>
+  );
+}
