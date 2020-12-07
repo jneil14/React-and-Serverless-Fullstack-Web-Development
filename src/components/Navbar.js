0b4ba48466f29839ee0c83f092ae8cc86c20b965
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-12-06 18:26:47 -0800

    Added auth from Auth0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import { StyledNavbar, StyledNavBrand, StyledNavItems, StyledLink } from '../styled/Navbar';
 import {Accent} from "../styled/Random";
-
+import { useAuth0 } from '../auth';
 
 export default function Navbar() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
     return (
       <StyledNavbar>
         <StyledNavBrand>
@@ -19,6 +20,16 @@ export default function Navbar() {
           <li>
             <StyledLink to="/HighScores">High Scores</StyledLink>
           </li>
+          {!isAuthenticated && (
+              <li>
+                <button onClick={loginWithRedirect}>Login</button>
+              </li>
+          )}
+          {isAuthenticated && (
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          )}
         </StyledNavItems>
       </StyledNavbar>
     );
--- src/index.js
+++ src/index.js
@@ -4,16 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ScoreProvider } from './contexts/ScoreContext';
+import { Auth0Provider } from './auth';
+import history from './utils/history';
+import config from './auth_config.json';
+import { configure } from 'airtable';
+
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-
-    <ScoreProvider>
-      <App />
-    </ScoreProvider>
-    
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+      audience={config.audience}
+    >
+      <ScoreProvider>
+        <App />
+      </ScoreProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
