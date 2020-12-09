403644e10ef097f0d740898890ecd1ad8e8efb04
Neil Solon <neilsolon@Neils-MacBook-Pro.local>
2020-12-08 21:07:19 -0800

    Included access token in saveHighScore call

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -3,10 +3,14 @@ import { useScore } from "../contexts/ScoreContext";
 import { StyledLink } from "../styled/Navbar";
 import { StyledCharacter } from "../styled/Game";
 import { StyledTitle } from "../styled/Random";
+import { useAuth0 } from "../auth";
+
 
 export default function GameOver({ history }) {
   const [score] = useScore();
   const [scoreMessage, setScoreMessage] = useState("");
+  const { getTokenSilently, isAuthenticated } = useAuth0();
+
   if (score === -1) {
     history.push("/");
   }
@@ -14,29 +18,41 @@ export default function GameOver({ history }) {
   useEffect(() => {
     const saveHighScore = async () => {
       try {
+        const token = await getTokenSilently();
         const options = {
           method: "POST",
-          body: JSON.stringify({ name: "Jhong", score })
+          body: JSON.stringify({ name: "Jhong", score }),
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
         };
         const res = await fetch("/.netlify/functions/saveHighScore", options);
         const data = await res.json();
-        if(data.id) {
-            setScoreMessage("Congrats!! You got a score worthy to be in the list!")
-        }
-        else {
-            setScoreMessage("Sorry, your score didn't quite make it to the list. Keep grinding.")
+        if (data.id) {
+          setScoreMessage(
+            "Congrats!! You got a score worthy to be in the list!"
+          );
+        } else {
+          setScoreMessage(
+            "Sorry, your score didn't quite make it to the list. Keep grinding."
+          );
         }
       } catch (err) {
         console.error(err);
       }
     };
-    saveHighScore();
-  }, [score]);
+
+    if (isAuthenticated) {
+      saveHighScore();
+    }
+  }, [isAuthenticated, getTokenSilently, score]);
 
   return (
     <div>
       <StyledTitle>Game Over</StyledTitle>
       <h2>{scoreMessage}</h2>
+      {!isAuthenticated &&
+      <h2>You should login or signup to compete for high scores! </h2>}
       <StyledCharacter>{score}</StyledCharacter>
       <div>
         <StyledLink to="/">Go Home</StyledLink>
